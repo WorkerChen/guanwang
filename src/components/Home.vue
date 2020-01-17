625d52bca068970e2a5af4dae43c72bcea25e015
DevZeng <zengshungeng@gmail.com>
2020-01-17 18:00:33 +0800

    fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -340,7 +340,7 @@ export default {
         font-weight: bolder;
         color: #fff;
         position: absolute;
-        bottom: 1.5rem;
+        bottom:0.5rem;
         right: 1.6rem;
       }
     }
@@ -437,7 +437,7 @@ export default {
           font-weight: bolder;
           color: #fff;
           position: absolute;
-          bottom: 1.5rem;
+          bottom: 0.5rem;
           right: 1.6rem;
         }
       }
